61660b9bc55f029e5b39f4c744374e3808b8ba1b
vampire-mayu <mayurmali31@gmail.com>
2022-01-09 13:47:25 +0530

    intial setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-5xl text-red-800 m-10 text-center">
+        welcome to blog app !
+      </h1>
+    </div>
+  );
+}
